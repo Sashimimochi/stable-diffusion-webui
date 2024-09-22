78ae56e87c12a1be0da2d4d5ddd41e498a6a4b98
sashimimochi <104ki.clark@gmail.com>
2024-09-22 23:09:32 +0900

    version up

--- launch.sh
+++ launch.sh
@@ -2,4 +2,6 @@
 
 genie -s
 sudo systemctl start docker
+DOCKER_BUILDKIT=1 /usr/local/bin/docker-compose --profile download up --build
+DOCKER_BUILDKIT=1 /usr/local/bin/docker-compose --profile auto up
 DOCKER_BUILDKIT=1 /usr/local/bin/docker-compose --profile auto up --build
